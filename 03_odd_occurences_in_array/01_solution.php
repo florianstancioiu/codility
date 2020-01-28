ee46a73782259053dc93442068e10d5e833275d1
Florin Victor Stancioiu <florinvictorstancioiu@gmail.com>
2020-01-28 03:23:07 +0200

    Add the third lesson

---
+++ 03_odd_occurences_in_array/01_solution.php
@@ -0,0 +1,17 @@
+/***
+
+    result: 44%
+
+***/
+
+function solution($a) {
+    while(sizeof($a) !== 0) {
+        $last_element = array_pop($a);
+
+        if ($key = array_search($last_element, $a) === false) {
+            return $last_element;
+        } else {
+            unset($a[$key]);
+        }
+    }
+}
---
+++ 03_odd_occurences_in_array/02_solution.php
@@ -0,0 +1,25 @@
+/***
+
+    result: 100%
+
+*/
+
+function solution($a) {
+    $array = [];
+
+    while (sizeof($a)) {
+        $element = array_pop($a);
+
+        if (! isset($array[$element])) {
+            $array[$element] = 0;
+        }
+
+        $array[$element]++;
+
+        if ($array[$element] % 2 === 0) {
+            unset($array[$element]);
+        }
+    }
+
+    return array_keys($array)[0];
+}
