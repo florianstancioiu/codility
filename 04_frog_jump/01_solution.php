dec2c0532f087851c34bd5a3e02b8072d44cbee1
Florin Victor Stancioiu <florinvictorstancioiu@gmail.com>
2020-01-28 03:46:05 +0200

    Add the fourth lesson

---
+++ 04_frog_jump/01_solution.php
@@ -0,0 +1,15 @@
+/***
+
+result: 44
+
+***/
+
+function solution($x, $y, $d) {
+    $increments = 0;
+    while($x < $y) {
+        $x += $d;
+        $increments++;
+    }
+    
+    return $increments;
+}
---
+++ 04_frog_jump/02_solution.php
@@ -0,0 +1,8 @@
+/***
+    result: 100
+
+***/
+
+function solution($x, $y, $d) {
+    return (int) ceil(($y - $x)/$d);
+}
