c0ea8f20bd1834373f208a08b3c8efb753b28339
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2020-02-24 23:07:06 +0200

    Add the 20th lesson

---
+++ 20_fish[37]/03_solution.php
@@ -0,0 +1,31 @@
+<?php
+// 37
+function solution($a, $b) {
+    $sum = null;
+    $last_key = null;
+    while ($sum !== 0 && $sum !== sizeof($b)) {
+        $sum = 0;
+        foreach ($b as $key => $val) {
+            if ($last_key === null || !isset($b[$last_key])) {
+                $last_key = $key;
+                continue;
+            }
+
+            if ($b[$last_key] === 1 && $val === 0) {
+                if ($a[$last_key] > $a[$key]) {
+                    unset($b[$key]);
+                    $sum += $b[$last_key];
+                } else {
+                    unset($b[$last_key]);
+                    $last_key = $key;
+                    $sum += $b[$key];
+                }
+                continue;
+            }
+
+            $last_key = $key;
+        }
+    }
+
+    return sizeof($b);
+}
